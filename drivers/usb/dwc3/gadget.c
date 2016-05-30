ea78607d7e891d35b61c903592ad4b8968017fd8
Felipe Balbi <felipe.balbi@linux.intel.com>
2016-05-30 17:13:16 +0300

    usb: dwc3: gadget: set xfer resource per endpoint
    Instead of looping through all endpoints when
    enabling ep0, let's allow for each endpoint to set
    its own xfer resource. This solves an issue which
    happens when we issue END_TRANSFER due to a reset
    interrupt. Endpoints will be left without a xfer
    resource to use.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -410,29 +410,20 @@ static int dwc3_gadget_start_config(struct dwc3 *dwc, struct dwc3_ep *dep)
 {
 	struct dwc3_gadget_ep_cmd_params params;
 	u32			cmd;
-	int			i;
 	int			ret;
 
-	if (dep->number)
-		return 0;
+	if (dep->number == 0) {
+		memset(&params, 0x00, sizeof(params));
+		cmd = DWC3_DEPCMD_DEPSTARTCFG;
 
-	memset(&params, 0x00, sizeof(params));
-	cmd = DWC3_DEPCMD_DEPSTARTCFG;
-
-	ret = dwc3_send_gadget_ep_cmd(dep, cmd, &params);
-	if (ret)
-		return ret;
-
-	for (i = 0; i < DWC3_ENDPOINTS_NUM; i++) {
-		struct dwc3_ep *dep = dwc->eps[i];
-
-		if (!dep)
-			continue;
-
-		ret = dwc3_gadget_set_xfer_resource(dwc, dep);
+		ret = dwc3_send_gadget_ep_cmd(dep, cmd, &params);
 		if (ret)
 			return ret;
 	}
+
+	ret = dwc3_gadget_set_xfer_resource(dwc, dep);
+	if (ret)
+		return ret;
 
 	return 0;
 }
