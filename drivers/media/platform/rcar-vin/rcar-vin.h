2875660750fa0edddbe27e674740bf84d5e54ff0
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-05-22 17:46:49 +0200

    rcar-vin: prepare for media controller mode initialization
    Prepare for media controller by calling a different initialization then
    for when running in device centric mode. Add trivial configuration of
    the mbus and creation of the media pad for the video device entity.
    
    While we are at it clearly mark the digital device centric notifier
    functions with a comment.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -101,6 +101,8 @@ struct rvin_info {
  * @notifier:		V4L2 asynchronous subdevs notifier
  * @digital:		entity in the DT for local digital subdevice
  *
+ * @pad:		media pad for the video device entity
+ *
  * @lock:		protects @queue
  * @queue:		vb2 buffers queue
  *
@@ -129,6 +131,8 @@ struct rvin_dev {
 	struct v4l2_ctrl_handler ctrl_handler;
 	struct v4l2_async_notifier notifier;
 	struct rvin_graph_entity *digital;
+
+	struct media_pad pad;
 
 	struct mutex lock;
 	struct vb2_queue queue;
