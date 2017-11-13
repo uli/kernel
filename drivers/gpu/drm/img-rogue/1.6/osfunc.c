78247d994ffea6f395e0042f0a2ca75d6ad4d002
Ulrich Hecht <ulrich.hecht+renesas@gmail.com>
2017-11-13 11:36:58 +0100

    drm/img-rogue: #include <linux/sched/clock.h> in osfunc.c
    Required for sched_clock()
    
    Signed-off-by: Ulrich Hecht <ulrich.hecht+renesas@gmail.com>

--- drivers/gpu/drm/img-rogue/1.6/osfunc.c
+++ drivers/gpu/drm/img-rogue/1.6/osfunc.c
@@ -69,6 +69,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endif
 #include <linux/kthread.h>
 #include <asm/atomic.h>
+#include <linux/sched/clock.h>
 #include <linux/pfn_t.h>
 
 #include "log2.h"
